909b63e72170b2f8905c3b32c552cfbea95d0a68
CTS-Corey <120518605+CTS-Corey@users.noreply.github.com>
2025-10-03 00:25:05 -0400

    fisrt

--- Node2d.cs
+++ Node2d.cs
@@ -6,6 +6,7 @@ public partial class Node2d : Node2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		Console.WriteLine("Node2d::_Ready()");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
